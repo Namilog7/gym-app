77a977210dc7c8e9cffa4ca4d87a7b3ebdca96f5
Gonzalo Baez <gonzaasd7@gmail.com>
2024-09-12 18:55:21 -0300

    diseño de dashadmin(ir haciendo sobre la marca, luego se cambiara)

---
+++ src/app/admin/components/NavAdmin.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+export const NavAdmin = () => {
+
+    return (
+        <nav className="navAdmin" >
+            <div>
+
+            </div>
+            <div>
+                <p>Miembros</p>
+                <p>Productos</p>
+                <form action="">
+                    <label htmlFor="search"></label>
+                    <input type="text" />
+                </form>
+            </div>
+        </nav>
+    )
+}
---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,9 @@
+import { NavAdmin } from "./components/NavAdmin";
+
+export default function Layout() {
+    return (
+        <>
+            <NavAdmin />
+        </>
+    )
+}
